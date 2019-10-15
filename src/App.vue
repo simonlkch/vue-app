c3bd7f1b7c9469eb1d843e2d49216dc2f2ba40bf
McCree <mccree.li@coreminder.com>
2019-10-15 12:32:29 +0800

    first init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+    <!-- <employee-form v-bind:employees="employees"/> -->
+    <employee-form @add:employee="addEmployee" />
+
+    <!-- <employee-table v-bind:employees="employees"/> -->
+    <employee-table 
+      :employees="employees"
+      @delete:employee="deleteEmployee"
+      @edit:employee="editEmployee"  
+     />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from '@/components/EmployeeTable.vue'
+import EmployeeForm from '@/components/EmployeeForm.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    EmployeeTable,
+    EmployeeForm
+  },
+  data(){
+    return{
+      employees:[
+        // {
+        //   id: 1,
+        //   name: 'sam',
+        //   email: 'sam@sam.com'
+        // },
+        // {
+        //   id: 2,
+        //   name: 'May',
+        //   email: 'may@may.com'
+        // },
+        // {
+        //   id: 3,
+        //   name: 'John',
+        //   email: 'john@john.com'
+        // }
+      ]
+    }
+  },
+  methods:{
+    addEmployee(employee){
+      const lastId = this.employees.length > 0 ? this.employees[this.employees.length-1].id : 0
+      const id = lastId + 1
+      const newEmployee = { ...employee,id}
+      this.employees = [...this.employees,newEmployee]
+    },
+    deleteEmployee(id){
+      this.employees = this.employees.filter(
+        employee => employee.id !== id
+      )
+    },
+    editEmployee(id, updatedEmployee) {
+      this.employees = this.employees.map(employee =>
+        employee.id === id ? updatedEmployee : employee
+      )
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  button{
+    background: #23a213;
+    border: 1px solid #23a213;
+  } 
+  .small-container{
+    max-width: 680px;
+  }
 </style>
